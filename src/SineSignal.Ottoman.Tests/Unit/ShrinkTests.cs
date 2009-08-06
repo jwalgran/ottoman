483649d0ef809a4ae93f816f516700b64e7cc19b
Dale Ragan <dale.ragan@sinesignal.com>
2009-08-06 16:17:35 -0400

    Finished validating the url given to Shrink for the CouchDB instance.

--- src/SineSignal.Ottoman.Tests/Unit/ShrinkTests.cs
+++ src/SineSignal.Ottoman.Tests/Unit/ShrinkTests.cs
@@ -34,20 +34,78 @@ namespace SineSignal.Ottoman.Tests.Unit
 		 */
 		[Test]
 		[ExpectedArgumentNullException]
-		public void Should_throw_invalid_argument_exception_if_url_is_null_or_empty_string()
+		public void Should_throw_argument_null_exception_when_url_is_null_or_empty_string()
 		{
-			// Arrange, Act, Assert
 			Shrink shrink = new Shrink(null);
+		}
+		
+		[Test]
+		[ExpectedArgumentException]
+		public void Should_throw_argument_exception_when_url_is_a_random_string()
+		{
+			Shrink shrink = new Shrink("some string value");
+		}
+		
+		[Test]
+		[ExpectedArgumentException]
+		public void Should_throw_argument_exception_when_url_is_relative()
+		{
+			Shrink shrink = new Shrink("../somepath");
+		}
+		
+		[Test]
+		[ExpectedArgumentException]
+		public void Should_throw_argument_exception_when_url_is_not_using_http_or_https_scheme()
+		{
+			string url = "ftp://127.0.0.1/somepath";
+			
+			Shrink shrink = new Shrink(url);
+
+			Assert.IsNotNull(shrink.CouchUri);
+			Assert.AreEqual(url, shrink.CouchUri.ToString());
+		}
+		
+		[Test]
+		public void Should_set_uri_for_couch_location_when_given_a_valid_http_uri()
+		{
+			string url = "http://127.0.0.1:5984/";
+			
+			Shrink shrink = new Shrink(url);
+
+			Assert.AreEqual(url, shrink.CouchUri.ToString());
+			Assert.AreEqual("http", shrink.CouchUri.Scheme);
+		}
+
+		[Test]
+		public void Should_set_uri_for_couch_location_when_given_a_valid_https_uri()
+		{
+			string url = "https://domain.com:5984/";
+			
+			Shrink shrink = new Shrink(url);
+
+			Assert.AreEqual(url, shrink.CouchUri.ToString());
+			Assert.AreEqual("https", shrink.CouchUri.Scheme);
 		}
 	}
 	
 	public class Shrink
 	{
-		public Uri CouchUri { get; protected set; }
+		private readonly Uri _couchUri;
 		
-		public Shrink(string url)
+		public Uri CouchUri { get { return _couchUri; } }
+		
+		public Shrink(string couchUrl)
 		{
-			if (String.IsNullOrEmpty(url)) throw new ArgumentNullException("url", "The value cannot be null or an empty string.");
+			// Validate input
+			if (String.IsNullOrEmpty(couchUrl))
+				throw new ArgumentNullException("couchUrl", "The value cannot be null or an empty string.");
+
+			bool isValidUri = Uri.TryCreate(couchUrl, UriKind.Absolute, out _couchUri);
+			if (!isValidUri)
+				throw new ArgumentException("The value is invalid, please pass a valid Uri.", "couchUrl");
+			
+			if (!_couchUri.Scheme.Equals("http") && !_couchUri.Scheme.Equals("https"))
+				throw new ArgumentException("The value is not using the http or https protocol.  This is not allowed since CouchDB uses REST and Http for communication.", "couchUrl");
 		}
 	}
 }
