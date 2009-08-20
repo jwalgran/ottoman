e2ed24f93c684818e46ba906d5685d904f741b68
Justin Walgran <justin@walgran.com>
2009-08-20 12:27:42 -0400

    Implemented the HiLoGenerator.

--- src/SineSignal.Ottoman.Tests/Unit/GeneratorTests.cs
+++ src/SineSignal.Ottoman.Tests/Unit/GeneratorTests.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Net;
 using Gallio.Framework;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
+using SineSignal.Ottoman;
+using SineSignal.Ottoman.Proxy;
 using SineSignal.Ottoman.Generators;
+using Moq;
+
 
 namespace SineSignal.Ottoman.Tests.Unit.Generators
 {
@@ -35,11 +40,86 @@ namespace SineSignal.Ottoman.Tests.Unit.Generators
     [TestFixture]
     public class When_creating_a_HiLoGenerator
     {
+        private HiLoGenerator _generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _generator = new HiLoGenerator();
+        }
+
         [Test]
         public void Should_have_a_default_ServerURL_option_set_to_localhost()
         {
+            Assert.AreEqual(_generator.Options["ServerURL"], "http://127.0.0.1:5984");
+        }
+
+        [Test]
+        public void Should_have_a_default_RestProxy_option_set_to_a_new_instance_of_RestProxy()
+        {
+            Assert.IsInstanceOfType<Ottoman.Proxy.RestProxy>(_generator.Options["RestProxy"]);
+        }
+    }
+
+    [TestFixture]
+    public class When_generating_an_ID_using_the_HiLoGenerator
+    {
+        private Mock<IRestProxy> _mockRestProxy;
+        private string _url;
+        private Uri _uuidURI;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _url = "http://127.0.0.1:5984/";
+            UriBuilder requestUriBuilder = new UriBuilder(_url);
+            string uuid = "0123456789abcdef0123456789abcdef";
+            string body = "{\"uuids\":[\"" + uuid + "\"]}";
+            requestUriBuilder.Path = "_uuids";
+            _uuidURI = requestUriBuilder.Uri;
+
+            var mockHttpResponse = new Mock<IHttpResponse>();
+            mockHttpResponse.Setup(x => x.StatusCode).Returns(HttpStatusCode.OK);
+            mockHttpResponse.Setup(x => x.Body).Returns(body);
+
+            _mockRestProxy = new Mock<IRestProxy>();
+            _mockRestProxy.Setup(x => x.Get(_uuidURI)).Returns(mockHttpResponse.Object);
+        }
+
+        [Test]
+        public void Should_request_a_UUID_from_CouchDB_only_the_first_time_Generate_is_called()
+        {
+            // Act
             var generator = new HiLoGenerator();
-            Assert.AreEqual(generator.Options["ServerURL"], "http://127.0.0.1:5984");
+            generator.Options["ServerURL"] = _url;
+            generator.Options["RestProxy"] = _mockRestProxy.Object;
+            generator.Generate();
+            generator.Generate();
+            generator.Generate();
+
+            // Assert
+            _mockRestProxy.Verify(x => x.Get(_uuidURI), Times.Once());
+        }
+
+        [Test]
+        public void Should_return_the_same_uuid_with_an_incrementing_appended_value_each_time_Generate_is_called()
+        {
+            // Arrange
+            string uuid = "0123456789abcdef0123456789abcdef";
+
+            // Act
+            var generator = new HiLoGenerator();
+            generator.Options["ServerURL"] = _url;
+            generator.Options["RestProxy"] = _mockRestProxy.Object;
+            var firstID = generator.Generate();
+            var secondID =  generator.Generate();
+            var thirdID =  generator.Generate();
+
+
+            Assert.AreEqual(uuid + "00000001", firstID);
+            Assert.AreEqual(uuid + "00000002", secondID);
+            Assert.AreEqual(uuid + "00000003", thirdID);
         }
     }
 }
--- src/SineSignal.Ottoman/Generators/HiLoGenerator.cs
+++ src/SineSignal.Ottoman/Generators/HiLoGenerator.cs
@@ -2,21 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace SineSignal.Ottoman.Generators
 {
+    /// <summary>
+    /// Used to generate document IDs seeded with a random value from a CouchDB then incremented on the client.
+    /// </summary>
     class HiLoGenerator : IGenerator
     {
-        public Dictionary<string, string> Options { get; set; }
+        public Dictionary<string, object> Options { get; set; }
 
+        private string _uuid = null;
+        private int _sequence = 0;
+
+        /// <summary>
+        /// Generates a unique document ID.
+        /// </summary>
+        /// <returns>A unique string each time the function is called.</returns>
         public string Generate()
         {
-            return Guid.NewGuid().ToString();
+            if (_uuid == null)
+            {
+                var proxy = (Ottoman.Proxy.IRestProxy)Options["RestProxy"];
+                var uuidURIBuilder = new UriBuilder((string)Options["ServerURL"]);
+                uuidURIBuilder.Path = "_uuids";
+                var response = proxy.Get(uuidURIBuilder.Uri);
+                JObject o = JObject.Parse(response.Body);
+                _uuid = o["uuids"][0].ToString().Replace("\"", "");
+            }
+            return _uuid + GetNextSequenceNumber();
+        }
+
+        private string GetNextSequenceNumber()
+        {
+            _sequence++;
+            return String.Format("{0:X}", _sequence).PadLeft(8,'0');
         }
 
         public HiLoGenerator()
         {
-            Options = new Dictionary<string, string> { {"ServerURL", "http://127.0.0.1:5984"} };
+            Options = new Dictionary<string, object> { {"ServerURL", "http://127.0.0.1:5984"},
+                                                       {"RestProxy", new Ottoman.Proxy.RestProxy(new Ottoman.Proxy.HttpClient())} };
         }
     }       
 }
