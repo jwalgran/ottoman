bde49b2a550648b44bdd5f2db6acd0a2d2c91e36
Justin Walgran <justin@walgran.com>
2009-09-10 18:10:34 -0400

    Added a framework for building the IDGenerator

---
+++ src/SineSignal.Ottoman/Generators/IDGenerator.cs
@@ -0,0 +1,31 @@
+﻿#region License
+
+// <copyright file="IDGenerator.cs" company="SineSignal, LLC.">
+//   Copyright 2007-2009 SineSignal, LLC.
+//       Licensed under the Apache License, Version 2.0 (the "License");
+//       you may not use this file except in compliance with the License.
+//       A copy of the License can be found in the LICENSE file or you may 
+//       obtain a copy of the License at
+//
+//           http://www.apache.org/licenses/LICENSE-2.0
+//
+//       Unless required by applicable law or agreed to in writing, software
+//       distributed under the License is distributed on an "AS IS" BASIS,
+//       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//       See the License for the specific language governing permissions and
+//       limitations under the License.
+// </copyright>
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SineSignal.Ottoman.Generators
+{
+    public class IDGenerator : IIDGenerator
+    {
+    }
+}
